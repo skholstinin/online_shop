27b359856d557dd724cd7bed1751d3e06ac5590f
sergey_work <s.kholstinin@yandex.ru>
2020-03-18 09:12:22 +0300

    Delete unused dependency & packages

--- backend/src/main/java/com/example/online_shop/OnlineShopApplication.java
+++ backend/src/main/java/com/example/online_shop/OnlineShopApplication.java
@@ -3,8 +3,6 @@ package com.example.online_shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
