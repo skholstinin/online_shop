bde75ba985c57c5b63238ed65b5c30472afdb9c2
Sergey <s.kholstinin@yandex.ru>
2020-03-18 20:20:40 +0300

    change application.properties

--- backend/src/main/java/com/example/onlineshop/OnlineShopApplication.java
+++ backend/src/main/java/com/example/onlineshop/OnlineShopApplication.java
@@ -3,12 +3,10 @@ package com.example.onlineshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "com.example.onlineshop.repository")
+@SpringBootApplication(scanBasePackages = "com.example.onlineshop")
 public class OnlineShopApplication {
 
     @Bean
