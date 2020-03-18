5a10bc23593bfcab21db3dc6b2f8a688a87ba3ca
Sergey <s.kholstinin@yandex.ru>
2020-03-18 17:57:09 +0300

    cnanfe java version to 8

--- backend/src/main/java/com/example/onlineshop/api/CartController.java
+++ backend/src/main/java/com/example/onlineshop/api/CartController.java
@@ -3,6 +3,7 @@ package com.example.onlineshop.api;
 
 import com.example.onlineshop.entity.Cart;
 import com.example.onlineshop.entity.ProductInOrder;
+import com.example.onlineshop.entity.ProductInfo;
 import com.example.onlineshop.entity.User;
 import com.example.onlineshop.form.ItemForm;
 import com.example.onlineshop.repository.ProductInOrderRepository;
@@ -53,7 +54,7 @@ public class CartController {
 
     @PostMapping("/add")
     public boolean addToCart(@RequestBody ItemForm form, Principal principal) {
-        var productInfo = productService.findOne(form.getProductId());
+        ProductInfo productInfo = productService.findOne(form.getProductId());
         try {
             mergeCart(Collections.singleton(new ProductInOrder(productInfo, form.getQuantity())), principal);
         } catch (Exception e) {
--- backend/src/main/java/com/example/onlineshop/service/impl/ProductInOrderServiceImpl.java
+++ backend/src/main/java/com/example/onlineshop/service/impl/ProductInOrderServiceImpl.java
@@ -1,14 +1,18 @@
 package com.example.onlineshop.service.impl;
 
 
+import com.example.onlineshop.entity.Cart;
+import com.example.onlineshop.entity.OrderMain;
 import com.example.onlineshop.entity.ProductInOrder;
 import com.example.onlineshop.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.example.onlineshop.repository.ProductInOrderRepository;
 import com.example.onlineshop.service.ProductInOrderService;
 
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Service
@@ -20,7 +24,7 @@ public class ProductInOrderServiceImpl implements ProductInOrderService {
     @Override
     @Transactional
     public void update(String itemId, Integer quantity, User user) {
-        var op = user.getCart().getProducts().stream().filter(e -> itemId.equals(e.getProductId())).findFirst();
+        Optional<ProductInOrder> op = user.getCart().getProducts().stream().filter(e -> itemId.equals(e.getProductId())).findFirst();
         op.ifPresent(productInOrder -> {
             productInOrder.setCount(quantity);
             productInOrderRepository.save(productInOrder);
@@ -30,7 +34,7 @@ public class ProductInOrderServiceImpl implements ProductInOrderService {
 
     @Override
     public ProductInOrder findOne(String itemId, User user) {
-        var op = user.getCart().getProducts().stream().filter(e -> itemId.equals(e.getProductId())).findFirst();
+        Optional<ProductInOrder> op = user.getCart().getProducts().stream().filter(e -> itemId.equals(e.getProductId())).findFirst();
         AtomicReference<ProductInOrder> res = new AtomicReference<>();
         op.ifPresent(res::set);
         return res.get();
